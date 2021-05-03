6b90e0dbd1ebac438fa8a2708dd83e276cf34819
Prasanta Panja <pp837@bath.ac.uk>
2021-05-03 19:51:03 +0000

    Renamed files

---
+++ CNN_BatchN.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, Callback, ReduceLROnPlateau
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.backend import clear_session
+
+import keras
+from keras import backend as K
+#from keras.regularizers import l2
+#from keras.callbacks import ModelCheckpoint
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+#from keras.optimizers import Adam
+
+def create_model(dropout):
+    model = Sequential([
+        Conv2D(32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(128, 128, 3)),
+        BatchNormalization(),
+	Conv2D(32, kernel_size=(3, 3), activation='relu'),
+        MaxPool2D(pool_size=(2, 2)),
+	Dropout(dropout),
+
+        Conv2D(64, kernel_size=(3, 3), activation='relu', padding='same'),
+	BatchNormalization(),
+        Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        MaxPool2D(pool_size=(2, 2)),
+	Dropout(dropout),
+
+        Conv2D(128, kernel_size=(3, 3), activation='relu', padding='same'),
+	BatchNormalization(),
+        Conv2D(128, kernel_size=(3, 3), activation='relu'),
+        Conv2D(128, kernel_size=(3, 3), activation='relu'),
+        MaxPool2D(pool_size=(2, 2)),
+	Dropout(dropout),
+
+        Conv2D(256, kernel_size=(3, 3), activation='relu', padding='same'),
+	BatchNormalization(),
+        Conv2D(256, kernel_size=(3, 3), activation='relu'),
+        Conv2D(256, kernel_size=(3, 3), activation='relu'),
+        MaxPool2D(pool_size=(2, 2)),
+	Dropout(dropout),
+
+        Flatten(),
+        Dense(17, activation='sigmoid') 
+    ])
+
+    return model
